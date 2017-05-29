56ac407ceb000baa4663404e1815a97320a63391
Nikolay <Filirien@abv.bg>
2017-05-29 19:39:51 +0300

    Stack and Queues - Exercise

---
+++ C# Advanced/Stacks and Queues - Exercise/06. Truck Tour/06. Truck Tour.cs
@@ -0,0 +1,57 @@
+﻿namespace _06.Truck_Tour
+{
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var fuelPoint = new long[n];
+            var distances = new long[n];
+            var successfulIndex = 0;
+            long fuel = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+                fuelPoint[i] = input[0];
+                distances[i] = input[1];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                fuel += fuelPoint[i];
+                fuel -= distances[i];
+                if (fuel - distances[i] < 0)
+                {
+                    if (i + 1 >= n)
+                    {
+                        Console.WriteLine("Impossible");
+                        break;
+                    }
+                    successfulIndex = i + 1;
+                    fuel = 0;
+                    continue;
+                }
+            }
+            if (successfulIndex == 0)
+            {
+                Console.WriteLine(successfulIndex);
+                return;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                fuel += fuelPoint[i];
+                fuel -= distances[i];
+                if (fuel - distances[i] < 0)
+                {
+                    Console.WriteLine("Impossible");
+                    return;
+                }
+            }
+            Console.WriteLine(successfulIndex);
+
+        }
+    }
+}
+
---
+++ C# Advanced/Stacks and Queues - Exercise/09. Stack Fibonacci/09. Stack Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿namespace _09.Stack_Fibonacci
+{
+    using System;
+    public class Program
+    {
+        public static long[] numbers;
+        public static void Main()
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            numbers = new long[n + 1];
+            long result = getFibonacci(n - 1);
+            Console.WriteLine(result);
+        }
+
+        private static long getFibonacci(int n)
+        {
+            if (n == 1 || n == 0)
+            {
+                return 1;
+            }
+            if (numbers[n] != 0)
+            {
+                return numbers[n];
+            }
+            numbers[n] = getFibonacci(n - 1) + getFibonacci(n - 2);
+            return numbers[n];
+        }
+    }
+}
