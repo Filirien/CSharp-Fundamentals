95bfedb6c359f26aa26e5ea872273e3283095a93
Nikolay <Filirien@abv.bg>
2017-06-11 15:19:27 +0300

    Manual String Processing - Lab

---
+++ C# Advanced/Manual String Processing - Lab/04. Special Words/Special Words.cs
@@ -0,0 +1,27 @@
+﻿namespace _04.Special_Words
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            char[] wordSeparators = new char[] { '(', ')', '[', ']', '<', '>', ',', '-', '!', '?', ' ' };
+            string[] specialWords = Console.ReadLine().Split(wordSeparators);
+            string[] textWords = Console.ReadLine().Split(wordSeparators);
+
+            foreach (var word in specialWords)
+            {
+                int numberOfOccurrences = 0;
+                foreach (var w in textWords)
+                {
+                    if (word == w)
+                    {
+                        numberOfOccurrences++;
+                    }
+                }
+
+                Console.WriteLine($"{word} - {numberOfOccurrences}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Manual String Processing - Lab/05. Concatenate Strings/Concatenate Strings.cs
@@ -0,0 +1,20 @@
+﻿namespace _05.Concatenate_Strings
+{
+    using System;
+    using System.Text;
+    public class Program
+    {
+        public static void Main()
+        {
+            int numberOfInputLines = int.Parse(Console.ReadLine());
+
+            StringBuilder resultText = new StringBuilder();
+            for (int i = 0; i < numberOfInputLines; i++)
+            {
+                resultText.Append(Console.ReadLine() + " ");
+            }
+
+            Console.WriteLine(resultText);
+        }
+    }
+}
